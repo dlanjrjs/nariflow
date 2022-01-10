1489e1b0c31ae05ad40ca69a4b470fdd4f503ee1
autonarimee <hoihoigogo@naver.com>
2022-01-10 23:43:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='nariflow',
-    version='0.0.3',
-    url='https://github.com/autonarimee/nariflow.git',
-    author='autonarimee',
+    version='0.1.0',
+    url='https://github.com/dlanjrjs/nariflow.git',
+    author='dlanjrjs',
     install_requires=[
         'numpy==1.20.1'
     ],
